b5cb956559e59f6cb4992118fc702367168453e2
isaachhk02 <isaac.fornos02@gmail.com>
2024-08-21 20:40:57 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,12 @@
+# A alternative Venom Installer by isaachhk02
+
+# EFI supported only!
+
+# BIOS Mode coming soon!
+
+# How to use?
+
+`sudo ./venominstaller.sh --help`
+
+# Syntax Example :
+`sudo ./venominstaller.sh /dev/nvme0n1 username`
---
+++ venominstaller.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Created by isaachhk02 
+#
+#  1.0.0
+#
+
+cat << EOF
+ _     _____ _      ____  _      _  _      ____  _____  ____  _     _     _____ ____ 
+/ \ |\/  __// \  /|/  _ \/ \__/|/ \/ \  /|/ ___\/__ __\/  _ \/ \   / \   /  __//  __
+| | //|  \  | |\ ||| / \|| |\/||| || |\ |||    \  / \  | / \|| |   | |   |  \  |  \/|
+| \// |  /_ | | \||| \_/|| |  ||| || | \||\___ |  | |  | |-||| |_/\| |_/\|  /_ |    /
+\__/  \____\_/  \|\____/\_/  \|\_/\_/  \|\____/  \_/  \_/ \|\____/\____/\_____/\_/  L
+                                                                                     
+ _  ____  ____  ____  ____  _     _     _  __ ____  ____                             
+/ \/ ___\/  _ \/  _ \/   _\/ \ /|/ \ /|/ |/ //  _ \/_   \                            
+| ||    \| / \|| / \||  /  | |_||| |_|||   / | / \| /   /                            
+| |\___ || |-||| |-|||  \_ | | ||| | |||   \ | \_/|/   /_                            
+\_/\____/\_/ \|\_/ \|\____/\_/ \|\_/ \|\_|\_\____/\____/
+EOF
+export USER="$2"
+export DEV="$1"
+
+export IS_NVME=0
+
+question=""
+
+Install() {
+	echo "Wiping $DEV"
+	wipefs -a "$DEV"
+	echo "Done!"
+	# Check if its a NVMe
+	if [[ $DEV == "/dev/nvme0*" ]]; then
+		echo "NVMe detected!"
+		IS_NVME=1
+	fi
+	echo "Creating GPT Table for ${DEV}"
+	parted -s "${DEV}" mklabel gpt
+	echo "Done!"
+
+	echo "Creating EFI partition on $DEV"
+	parted -s "${DEV}" mkpart primary efi fat32 1MiB 1024MiB
+	echo "Setting as EFI!"
+	parted -s "${DEV}" set 1 esp on
+	echo "Done!"
+	echo "Creating root partition on $DEV"
+	parted -s "${DEV}" mkpart primary root ext4 1MiB 100%
+	echo "Done!"
+
+	mkdir -p /mnt/venom/boot
+	echo "Mounting partitions to /mnt/venom!"
+	if [[ "$IS_NVME" -eq 1 ]]; then
+		mount "$DEV"p1 /mnt/venom/boot
+		mount "$DEV"p2 /mnt/venom
+	else
+		mount "$DEV"1 /mnt/venom/boot
+		mount "$DEV"2 /mnt/venom
+	fi
+	echo "Installing Venom Linux"
+	unsquashfs -v -f -d /mnt/venom /run/initramfs/medium/rootfs/filesystem.sfs
+	echo "Done!"
+	git clone https://github.com/archlinux/arch-install-scripts.git
+	cd "arch-install-scripts" || return
+	make install
+	arch-chroot /mnt/venom grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Venom
+	genfstab -U /mnt/venom > /mnt/venom/etc/fstab
+	echo "Creating user: $USER"
+	arch-chroot /mnt/venom useradd -m "$USER"
+	arch-chroot /mnt/venom passwd "$USER"
+	echo "Created successfully!"
+
+	arch-chroot /mnt/venom usermod -aG wheel "$USER"
+	echo "Added to wheel group"
+	arch-chroot /mnt/venom usermod -aG sudo "$USER"
+	echo "Removing live user!"
+
+	arch-chroot /mnt/venom userdel -r venom
+
+	umount -l -f "$DEV"
+
+	echo "Write reboot to reboot your computer and cross your fingers!"
+}
+
+
+
+if [[ $EUID -eq 0 ]]; then
+	if [[ -z $1 || -z $2 ]]; then
+		echo "Syntax: venominstaller [device] [user]"
+		echo "ERROR: No arguments passed!"
+	else
+
+		echo "Selected device: $DEV"
+		echo "Selected user: $USER"
+		read -r -p "Are you sure to proceed? THIS ERASE YOUR DISK ENTIRELY AND ERASE THE DATA! (y/n) " "question"
+		if [ "${question}" == "" ]; then
+			echo "Aborted!"
+			exit
+		elif [ "${question}" == "y" ]; then
+			clear
+			echo "Starting installer"
+			Install
+		else
+			echo "Aborted"
+			exit
+		fi
+	fi
+	if [[ $1 == "--help" || $1 == "-h" ]]; then
+		echo "Syntax: venominstaller [device] [user]"
+	fi
+else
+	echo "ERROR: Run as root!"
+fi
